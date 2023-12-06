c5f11b1256493752a7c9e7b7f58915c8c8afa66b
Jabahahaha <misha.andreev3004@gmail.com>
2023-12-06 04:21:20 +0100

    first commit

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import random
-
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 import pygame
 
 app = Flask(__name__)
 
-@app.route("/"  )
-def hello_world():
-    pygame.init()
-    screen = pygame.display.set_mode((300, 300))
+pygame.init()
+screen = pygame.display.set_mode((300, 300))
 
-    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+@app.route("/")
+def hello_world():
+    default_radius = random.randint(10, 100)
+    default_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    default_x = random.randint(0, 255)
+    default_y = random.randint(0, 255)
+
+    # Getting parameters from the URL
+    radius = request.args.get('radius', default=default_radius, type=int)
+    color = request.args.get('color', default=default_color)
+    x = request.args.get('x', default=default_x, type=int)
+    y = request.args.get('y', default=default_y, type=int)
+
+    if isinstance(color, str):
+        color = tuple(map(int, color.split(',')))
 
     screen.fill((0,0,0))
-    pygame.draw.circle(screen, color, (150, 150), 50)
+    pygame.draw.circle(screen, color, (x, y), radius)
     pygame.display.flip()
 
     image_name = "screen.jpg"
@@ -22,12 +32,4 @@ def hello_world():
 
     return render_template('generated-image.html', image_file=image_name)
 
-
-@app.route("/say-hi/<name>")
-def say_hi(name):
-    # my_name = input("Enter your name: ")
-    return f"<b>Hi, {name} from {my_name}!</b>"
-
-@app.route("/board")
-def show_board():
-    return render_template('board-page.html', name="Serhii")
+# Other routes remain the same
