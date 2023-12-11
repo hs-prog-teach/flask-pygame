64cf310afd9bdeecaaf1d4640caed72d2b07e810
ZainLatif7164 <latifzain40@gmail.com>
2023-12-11 13:43:36 +0100

    Video Homework

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 import random
 from flask import Flask, render_template
-
-import pygame
+from PIL import Image, ImageDraw
+from moviepy.editor import ImageSequenceClip
 
 app = Flask(__name__)
 
-@app.route("/"  )
+@app.route("/")
 def hello_world():
-    pygame.init()
-    screen = pygame.display.set_mode((300, 300))
+    frames = []
+
+    for _ in range(30):
+        img = generate_image()
+        frames.append(img)
+
+    image_names = save_frames(frames)
+
+    video_name = "generated_video.mp4"
+    create_video(image_names, video_name)
+
+    return render_template('generated-video.html', video_file_name=video_name)
+
+def generate_image():
+    image = Image.new("RGB", (300, 300), (0, 0, 0))
+    draw = ImageDraw.Draw(image)
 
     color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    draw.ellipse((100, 100, 200, 200), fill=color)
 
-    screen.fill((0,0,0))
-    pygame.draw.circle(screen, color, (150, 150), 50)
-    pygame.display.flip()
+    return image
 
-    image_name = "screen.jpg"
-    pygame.image.save(screen, f"static/{image_name}")
+def save_frames(frames):
+    image_names = []
 
-    return render_template('generated-image.html', image_file=image_name)
+    for i, frame in enumerate(frames):
+        image_name = f"frame_{i}.png"
+        frame.save(f"static/{image_name}")
+        image_names.append(f"static/{image_name}")
 
+    return image_names
 
-@app.route("/say-hi/<name>")
-def say_hi(name):
-    # my_name = input("Enter your name: ")
-    return f"<b>Hi, {name} from {my_name}!</b>"
+def create_video(image_names, video_name):
+    clip = ImageSequenceClip(image_names, fps=10)
+    clip.write_videofile(f"static/{video_name}", codec="libx264", audio=False)
 
-@app.route("/board")
-def show_board():
-    return render_template('board-page.html', name="Serhii")
+# @app.route("/say-hi/<name>")
+# def say_hi(name):
+#     return f"<b>Hi, {name}!</b>"
+
+# @app.route("/board")
+# def show_board():
+#     return render_template('board-page.html', name="Serhii")
